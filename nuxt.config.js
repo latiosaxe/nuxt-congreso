285198984015a047841d140d5b84a6f8b581cc0e
Axel González <axel@halfhelix.com>
2022-03-25 22:38:19 -0600

    Removing swiper

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,8 +21,7 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
-    '~/assets/css/main.scss',
-    'node_modules/swiper/swiper.min.css'
+    '~/assets/css/main.scss'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
